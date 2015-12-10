aeeb06ee2d69e4f0a2c797d04b6157826934990e
peteraldaron <peteraldaron@gmail.com>
2015-12-10 14:03:25 -0500

    upadte

--- main.py
+++ main.py
@@ -4,27 +4,84 @@ from numpy import linalg
 from scipy import fftpack
 from pylab import *
 
-def pipeline():
-    syllables = fileio.readAllFilesInDirectory("./audio/syllables/")
-    sr = syllables[0][0].sr
-    features = [(feature.fft_extract(x[0].data), x[1]) for x in syllables]
-    for i,j in features:
-        title(j)
-        plot(i)
-        print(feature.fft_to_freq(i, sr))
-        show()
+TEST_DIR = "./audio/testdata/"
+WORD_DIR = "./audio/"
+SYLLABLE_DIR = "./audio/syllables/"
 
+vocab = set([
+"suomi",
+"synnyinmaa",
+"sana",
+"kultainen",
+"laaksoa",
+"kukkulaa",
+"ei",
+"rantaa",
+"rakkaampaa",
+"kuin",
+"kotimaa",
+"pohjoinen",
+"maa",
+"kallis",
+"isien"
+ ])
 
-#pipeline()
+vocab_syllables = { 
+"ei":1,
+"isien":4,
+"kallis":4,
+"kotimaa":4,
+"kuin":3,
+"kukkulaa":4,
+"kultainen":6,
+"laaksoa":5,
+"maa":2,
+"pohjoinen":6,
+"rakkaampaa":6,
+"rantaa":4,
+"sana":2,
+"suomi":3,
+"synnyinmaa":7,
+}
+
+syllables_to_vocab ={}
+for i in range(max(map(lambda l :l, vocab_syllables.values()))):
+    syllables_to_vocab[i] = []
+
+for i in vocab:
+    syllables_to_vocab[vocab_syllables[i]] = i
+
+def guess_syllables(wave, window_size=10):
+    amp, freq = syllable.segmentation(wave)
+    windowed_amp = syllable.moving_window(amp/linalg.norm(amp), window_size)
+    maximi = syllable.local_max_locations(windowed_amp)
+    return len(maximi[0])
+
+def syllable_debug_test():
+    audiofiles = fileio.readAllFilesInDirectory("./audio/testdata/")
+    sr = audiofiles[0][0].sr
+    for file in audiofiles:
+        print(file[1]+" guess: "+str(guess_syllables(file[0])))
+        print(file[1]+ " actual: "+str(vocab_syllables[file[1]]))
 
 def single_wav_analysis():
     audiofiles = fileio.readAllFilesInDirectory("./audio/")
     sr = audiofiles[0][0].sr
     for file in audiofiles:
-        print(feature.dct2(file[0].data, sr, topn=3))
         title(file[1])
         amp, freq = syllable.segmentation(file[0])
-        plot(amp/linalg.norm(amp))
-        plot(freq/linalg.norm(freq))
+        windowed_amp = syllable.moving_window(amp/linalg.norm(amp), 10)
+        plot(windowed_amp, label="amp")
+        #plot(syllable.moving_average(freq/linalg.norm(freq), 10),label="freq")
+        plot(np.repeat(np.mean(windowed_amp),len(amp)), label='avga')
+        maximi = syllable.local_max_locations(windowed_amp)
+        print(maximi)
+        #print(syllable.filter_distinct_peaks(windowed_amp, maximi))
+        #plot(np.repeat(np.mean(freq/linalg.norm(freq)),len(amp)),label='avgf')
         show()
-single_wav_analysis()
+        #title(file[1])
+        #plot(freq)
+        #avgfreq = np.mean(freq)
+        #plot(np.repeat(avgfreq, len(freq)))
+#single_wav_analysis()
+syllable_debug_test()
--- syllable.py
+++ syllable.py
@@ -1,240 +1,44 @@
 import numpy as np, scipy
-import pandas as pd
+from scipy import signal
 import math
 import fileio, feature
-
-'''
-implemented based on description in
-Xie, Niyogi (2006)
-'''
-def sample_autocovar(samples, h):
-    upper_bound = len(samples) - h
-    return (1/n)*sum(map(lambda t: samples[t+h]*samples[t], range(0, upper_bound)))
-
-#samples: 1-d np array
-#todo: optimize
-def periodicity(samples, p):
-    n = len(samples)
-    return (sample_autocovar(samples, p)/(n-p))/(sample_autocovar(samples, 0)/n)
-
-def frame_energy(samples):
-    return math.log(sample_autocovar(samples, 0), 2)
-
-'''
-end X&N
-'''
+from scipy.signal import argrelextrema
 
 #moving average:
-def mean_amplitude(samples, length):
+def moving_average(samples, length):
     #add padding:
-    return np.hstack((np.zeros(length-1), pd.rolling_mean(np.absolute(samples),
-            length)[length-1:]))
+    return np.convolve(samples, np.ones(length)/length)[:len(samples)]
 
-def coarse_mean_amplitude(samples, length):
-    return np.hstack(map(lambda x: np.ones(length)*x,
-        np.mean(np.hstack((samples, samples[-1]*np.ones(length-(len(samples)%length))))
-            .reshape(-1, length), axis=1)))
+#moving average:
+def moving_window(samples, length):
+    #add padding:
+    return np.convolve(samples, signal.windows.hann(length))[:len(samples)]
+    
+def local_max_locations(sample):
+    #note: discrete...can't do zeros
+    '''
+    first_order = np.diff(sample)
+    second_order = np.diff(sample, n=2)
+    #where second order is negative
+    return np.intersect1d(np.where(first_order==0), np.where(second_order < 0))
+    '''
+    return argrelextrema(sample, np.greater)
 
+def top_n_local_max_locations(sample, n=5):
+    #return locations
+    locs = local_max_locations(sample)
+    return np.sort(locs[sample[locs].argsort()[::-1][:n]])
 
-dip = set([
-    "k",
-    "p",
-    "t"
-    #'l'
-]);
-
-mult_dip = set([
-    "kk",
-    "pp",
-    "tt"
-])
-
-consonants = set([
-    "b",
-    "c",
-    "d",
-    "f",
-    "g",
-    "h",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "v",
-    "w",
-    "x",
-    "z"
-])
-
-vowels = set([
-    "a",
-    "o",
-    "i",
-    "e",
-    "u",
-    "ä",
-    "ö",
-    "y"
-])
-
-
-#reimplemented from c++
-def tokenize_syllable(word):
-    wordlen = len(word)
-
-    tokens = []
-
-    def is_dip(w):
-        return w in dip
-
-    def is_consonant(w):
-        return w in consonants
-
-    def is_vowel(w):
-        return w in vowels
-
-    if (len(word) == 0):
-        return tokens
-
-    i = 0
-
-
-    while i < wordlen:
-        if (i == 0):
-            curr = word[i];
-            buff = word[i];
-            if (is_consonant(curr)):
-                #look ahead:
-                if (i+1 < wordlen and is_consonant(word[i+1])):
-                    #if (is_vowel(word[i+2])):
-                    #    tokens.append(buff);
-                    #    i = i+1; continue
-                    #else:
-                    #    print("virhe!")
-                    #    i=i+1; continue;
-
-                    tokens.append(buff);
-                    i = i+1; continue
-                elif (i+1 <wordlen and is_vowel(word[i+1])):
-                    buff += word[i+1];
-                    tokens.append(buff);
-                    i = i+1;
-                    i=i+1; continue;
-                else:
-                    print("virhe!")
-                    i = i+1
-            elif (is_vowel(curr)):
-                #look ahead:
-                if (i+1 < wordlen) :
-                    #next is vowel:
-                    if (is_vowel(word[i+1])) :
-                        tokens.append(buff);
-                        i=i+1; continue;
-                    elif (is_consonant(word[i+1])) :
-                        #look further ahead:
-                        if (i+2 < wordlen and word[i+1] == word[i+2]):
-                            if (is_dip(word[i+1])) :
-                                #ditch, next iteration should pick up.
-                                #a-kk-u, a-pp-u, a-tt-u
-                                tokens.append(buff);
-                                i=i+1; continue;
-                            else:
-                                #lex next as token:
-                                buff += word[i+1];
-                                tokens.append(buff);
-                                i = i+1;
-                                i=i+1; continue;
-                        #if is consonant and next is not equal:
-                        else:
-                            if (i+2 < wordlen and is_consonant(word[i+2])):
-                                #lex next as token:
-                                buff += word[i+1];
-                                tokens.append(buff);
-                                i = i+1;
-                                i=i+1; continue;
-                            else :
-                                tokens.append(buff);
-                                i=i+1; continue;
-                #last vowel, return.
-                else :
-                    tokens.append(buff);
-                    i=i+1; continue;
-        #not first symbol
-        else:
-            curr = word[i];
-            buff = word[i];
-            if (is_consonant(curr)):
-                #look ahead:
-                if (i+1<wordlen):
-                    if (is_consonant(word[i+1])):
-                        if (i+2 < wordlen and is_vowel(word[i+2]) and curr == word[i+1] and
-                                is_dip(curr)) :
-                            #only case for long consonants:
-                            buff += word[i+1];
-                            buff += word[i+2];
-                            tokens.append(buff);
-                            i = i+2;
-                            i=i+1; continue;
-                        #otherwise just add:
-                        else :
-                            tokens.append(buff);
-                            i=i+1; continue;
-                    if (is_vowel(word[i+1])):
-                        #lex whole thing
-                        buff += word[i+1];
-                        tokens.append(buff);
-                        i = i+1;
-                        i=i+1; continue;
-                else :
-                    tokens.append(buff);
-                    i=i+1; continue;
-            elif (is_vowel(curr)) :
-                #look ahead:
-                if (i+1 < wordlen) :
-                    #next is vowel:
-                    if (is_vowel(word[i+1])) :
-                        tokens.append(buff);
-                        i=i+1; continue;
-                    elif (is_consonant(word[i+1])) :
-                        #look further ahead:
-                        if (i+2 < wordlen and word[i+1] == word[i+2] and is_dip(word[i+1])) :
-                            #ditch, next iteration should pick up.
-                            #a-kk-u, a-pp-u, a-tt-u
-                            tokens.append(buff);
-                            i=i+1; continue;
-                        #if is consonant and next is not equal:
-                        else :
-                            tokens.append(buff);
-                            i=i+1; continue
-                #last vowel, return.
-                else :
-                    tokens.append(buff);
-                    i=i+1; continue;
-    return tokens
-
-#assuming trimmed audio on both ends
-#wave: waveData or wave
-def waveDivisionBySyllable(wave, word):
-    tokens = tokenize_syllable(word);
-    wordlen = len(word)
-    per_letter_len = wave.data.size / wordlen
-    frame_marks = np.hstack((np.zeros(1), np.array([len(x) for x in tokens]).cumsum() * per_letter_len))
-    return list(map(lambda x : {
-            'token': x[2],
-            'samples':fileio.WaveData(wave.data[int(x[0]):int(x[1])], wave.sr)
-        }, zip(frame_marks[:-1], frame_marks[1:], tokens)))
-
-
+def filter_distinct_peaks(samples, max_locations, filtering_percentage=0.03):
+    #forward looking:
+    smax = max(samples)
+    return [max_locations[i] for i in range(len(max_locations)-1)
+        if abs((samples[max_locations[i]]-samples[max_locations[i+1]])/smax) >= filtering_percentage]
+            
 #attempts to segment a given input through frequency/amplitude analysis:
-def segmentation(wave):
+def segmentation(wave, seglen=256):
     sr = wave.sr
-    fft_result = [feature.fft_extract(x) for x in fileio.segment(wave.data, 256)]
+    fft_result = [feature.fft_extract(x) for x in fileio.segment(wave.data, seglen)]
     freq_at_each_step = [feature.fft_to_freq(fft_res, sr, topn=1) for fft_res in fft_result]
     amp_at_each_step = [x.real.max() for x in fft_result]
     return amp_at_each_step, np.hstack(freq_at_each_step)
